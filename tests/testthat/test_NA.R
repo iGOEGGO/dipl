a23842f63b45684dbe58ea55658bb68d1e331a64
jborensky <jborensky@student.tgm.ac.at>
2021-03-17 23:44:38 +0100

    add tests

--- tests/testthat/test_NA.R
+++ tests/testthat/test_NA.R
@@ -1,12 +1,12 @@
 library(stringr)
 
-test_that("delete NAs", {
+test_that("delete Columns with only NA values", {
   dataset <- read.csv("testfile_1.csv")
   dataset <- removeNAColumns(dataset)
   expect_equal(length(colnames(dataset)), 7)
 })
 
-test_that("fill NAs", {
+test_that("fill empty cells with NA", {
   dataset <- read.csv("testfile_1.csv")
   indices <- c(0)
   for (i in 1:ncol(dataset)) {
--- tests/testthat/test_strCols.R
+++ tests/testthat/test_strCols.R
@@ -1,5 +1,17 @@
 library(stringr)
 
+test_that("convert factor-Columns", {
+  dataset <- read.csv("data_STROKE.csv", fileEncoding = "latin1")
+  n1 <- names(Filter(is.factor, dataset))
+  dataset <- resetFactors(dataset)
+  n2 <- names(Filter(is.factor, dataset))
+  # Daten werden hier auch gelöscht -> noch strings
+  print("Hallo")
+  print(length(n1))
+  print(length(n2))
+  expect_true(isTRUE(all.equal(length(n1), length(n2))))
+})
+
 test_that("delete String-Columns", {
   dataset <- read.csv("testfile_1.csv")
   dataset <- filterStrings(dataset)
